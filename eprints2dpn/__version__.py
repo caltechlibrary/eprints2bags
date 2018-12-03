3afb329bcc83fa17ef5342e8653e15a0aed360d5
Michael Hucka <mhucka@caltech.edu>
2018-12-03 15:24:50 -0800

    Add typical __init__ and __version__ files.

---
+++ eprints2dpn/__version__.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Eprints2dpn version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/eprints2dpn
+# =============================================================================
+
+__version__     = '1.0.0'
+__title__       = 'eprints2dpn'
+__name__        = 'Eprints2dpn'
+__description__ = '''Package up CODA materials for deposition into DPN.'''
+__url__         = 'https://github.com/caltechlibrary/eprints2dpn'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>, Betsy Coles <betsycoles@gmail.com>'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause license -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
