4a123fbd545a32729a551ae6630a1a1d5e3bdfa2
Michael Hucka <mhucka@caltech.edu>
2018-12-04 14:30:42 -0800

    Split out eprints utilities into a separate file

---
+++ eprints2dpn/eprints.py
@@ -0,0 +1,86 @@
+'''
+eprints.py: Eprints-specific utilities
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2018 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from   collections import defaultdict
+import lxml.etree as etree
+import os
+from   os import path
+import shutil
+
+
+# Main functions.
+# .............................................................................
+
+def eprint_dc_dict(dc_text):
+    '''Take a block of text representing Dublin Core, and return a dict.'''
+    # Each item is a string representing DC info, with embedded newlines.  We
+    # split on the newlines, then split again on the ':' and enter the pairs
+    # from the ':' split into a dictionary.
+    record = defaultdict(list)
+    for pair in [line.split(':', 1) for line in dc_text.split('\n')]:
+        if len(pair) == 2:
+            record[pair[0].strip()].append(pair[1].strip())
+    return record
+
+
+def eprint_xml(number, epxml):
+    node = epxml.find(xpath_for_record(number))
+    if node is None:
+        exit('Cannot find {} in XML file'.format(number))
+    else:
+        return node
+
+
+def eprint_number(record):
+    # There is no explicit eprints identifier in the DC format (why not?), so
+    # we have to get it from the URL stored as one of the 'relation' values.
+    # This will be a string like 'https://authors.library.caltech.edu/4/'.
+    if 'relation' not in record:
+        print('Record without a "relation" value: {}'.format(record['title']))
+        return ''
+    for value in record['relation']:
+        if value.startswith('https://authors.library.caltech.edu'):
+            # Remove trailing slash and grab the number at the end.
+            return value.strip('/').split('/')[-1]
+
+
+def eprint_documents(xml):
+    files = []
+    for document in xml.findall('.//{http://eprints.org/ep2/data/2.0}documents'):
+        for url in document.findall('.//{http://eprints.org/ep2/data/2.0}url'):
+            files.append(url.text)
+    return files
+
+
+def write_record(number, dc, xml, base_name, dir_path):
+    # Write out a text file containing the DC content.
+    dc_file_name = base_name + '-' + str(number) + '-DC.txt'
+    with open(path.join(dir_path, dc_file_name), 'w') as file:
+        file.write(dc)
+
+    # Write out another file containing the XML content.
+    xml_file_name = base_name + '-' + str(number) + '.xml'
+    with open(path.join(dir_path, xml_file_name), 'w') as file:
+        file.write("<?xml version='1.0' encoding='utf-8'?>\n")
+        file.write("<eprints xmlns='http://eprints.org/ep2/data/2.0'>\n")
+        file.write('  ' + etree.tostring(xml, encoding='UTF-8').decode().rstrip() + '\n')
+        file.write("</eprints>")
+
+
+def xpath_for_record(number):
+    ns = '{http://eprints.org/ep2/data/2.0}'
+    prefix = 'https://authors.library.caltech.edu/id/eprint'
+    return './/{}eprint[@id="{}/{}"]'.format(ns, prefix, number)
