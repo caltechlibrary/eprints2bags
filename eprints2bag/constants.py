ca60363435586b0347d20ab8774bae8c182d39a7
Michael Hucka <mhucka@caltech.edu>
2018-12-04 14:38:14 -0800

    Add constants.py

---
+++ eprints2bag/constants.py
@@ -0,0 +1,19 @@
+'''
+constants: global constants for Eprints2bag.
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2018 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+import sys
+
+ON_WINDOWS = sys.platform.startswith('win')
