e742004eca28faa8500384535d2ac2f7a2feccd9
Michael Hucka <mhucka@caltech.edu>
2018-12-04 14:32:17 -0800

    Update for new name & purpose

--- eprints2bag/__version__.py
+++ eprints2bag/__version__.py
@@ -1,16 +1,16 @@
 # =============================================================================
 # @file    __version__.py
-# @brief   Eprints2dpn version info
+# @brief   Eprints2bag version info
 # @author  Michael Hucka <mhucka@caltech.edu>
 # @license Please see the file named LICENSE in the project directory
-# @website https://github.com/caltechlibrary/eprints2dpn
+# @website https://github.com/caltechlibrary/eprints2bag
 # =============================================================================
 
 __version__     = '1.0.0'
-__title__       = 'eprints2dpn'
-__name__        = 'Eprints2dpn'
-__description__ = '''Package up CODA materials for deposition into DPN.'''
-__url__         = 'https://github.com/caltechlibrary/eprints2dpn'
+__title__       = 'eprints2bag'
+__name__        = 'Eprints2bag'
+__description__ = '''Package up CODA materials as BagIt bags.'''
+__url__         = 'https://github.com/caltechlibrary/eprints2bag'
 __author__      = 'Michael Hucka <mhucka@caltech.edu>, Betsy Coles <betsycoles@gmail.com>'
 __email__       = 'mhucka@caltech.edu'
 __license__     = 'BSD 3-clause license -- see LICENSE file'
