a76d558b00f2cba3c50ac266408d7a26da9e4d24
Michael Hucka <mhucka@caltech.edu>
2018-12-07 12:02:38 -0800

    Version bump for bug fix

--- eprints2bags/__version__.py
+++ eprints2bags/__version__.py
@@ -6,7 +6,7 @@
 # @website https://github.com/caltechlibrary/eprints2bags
 # =============================================================================
 
-__version__     = '1.0.1'
+__version__     = '1.0.2'
 __title__       = 'eprints2bags'
 __name__        = 'eprints2bags'
 __description__ = '''Package up EPrints materials as BagIt bags.'''
