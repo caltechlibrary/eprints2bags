4143421cbd0dc4920ad5d8b9a700515dbdc4e295
Michael Hucka <mhucka@caltech.edu>
2018-12-10 17:17:45 -0800

    Add definition of keyring used by eprints2bags

--- eprints2bags/constants.py
+++ eprints2bags/constants.py
@@ -17,3 +17,7 @@ file "LICENSE" for more information.
 import sys
 
 ON_WINDOWS = sys.platform.startswith('win')
+'''True if we're running on a Windows system, False otherwise.'''
+
+KEYRING = "org.caltechlibrary.eprints2bags"
+'''The name of the keyring used to store Caltech access credentials, if any.'''
