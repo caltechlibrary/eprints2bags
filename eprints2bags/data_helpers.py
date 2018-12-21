6d49c9239ce606e9fb49245cf4c712263fb786da
Michael Hucka <mhucka@caltech.edu>
2018-12-21 12:34:45 -0800

    Add new file of data helper functions

---
+++ eprints2bags/data_helpers.py
@@ -0,0 +1,41 @@
+'''
+data_helpers: data manipulation utilities
+'''
+
+# Based on http://stackoverflow.com/a/10824484/743730
+def flatten(iterable):
+    '''Flatten a list produced by an iterable.  Non-recursive.'''
+    iterator, sentinel, stack = iter(iterable), object(), []
+    while True:
+        value = next(iterator, sentinel)
+        if value is sentinel:
+            if not stack:
+                break
+            iterator = stack.pop()
+        elif isinstance(value, str):
+            yield value
+        else:
+            try:
+                new_iterator = iter(value)
+            except TypeError:
+                yield value
+            else:
+                stack.append(iterator)
+                iterator = new_iterator
+
+
+def ordinal(n):
+    '''Print a number followed by "st" or "nd" or "rd", as appropriate.'''
+    # Spectacular algorithm by user "Gareth" at this posting:
+    # http://codegolf.stackexchange.com/a/4712
+    return '{}{}'.format(n, 'tsnrhtdd'[(n/10%10!=1)*(n%10<4)*n%10::4])
+
+
+def expand_range(text):
+    '''Return individual numbers for a range expressed as X-Y.'''
+    # This makes the range 1-100 be 1, 2, ..., 100 instead of 1, 2, ..., 99
+    if '-' in text:
+        range_list = text.split('-')
+        return [*map(str, range(int(range_list[0]), int(range_list[1]) + 1))]
+    else:
+        return text
