d29fe0d38474331cdf24febceebd377aea1c8918
Michael Hucka <mhucka@caltech.edu>
2019-09-12 14:33:03 -0700

    Move almost all metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,53 +1,26 @@
 #!/usr/bin/env python3
 # =============================================================================
 # @file    setup.py
-# @brief   eprints2bags setup file
+# @brief   Installation setup file
 # @author  Michael Hucka <mhucka@caltech.edu>
 # @license Please see the file named LICENSE in the project directory
 # @website https://github.com/caltechlibrary/eprints2bags
+#
+# Note: configuration metadata is maintained in setup.cfg.  This file exists
+# primarily to hook in setup.cfg and requirements.txt.
+#
 # =============================================================================
 
 import os
 from   os import path
 from   setuptools import setup
-import sys
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'requirements.txt')) as f:
     reqs = f.read().rstrip().splitlines()
 
-with open("README.md", "r", errors = 'ignore') as f:
-    readme = f.read()
-
-# The following reads the variables without doing an "import eprints2bags",
-# because the latter will cause the python execution environment to fail if
-# any dependencies are not already installed -- negating most of the reason
-# we're using setup() in the first place.  This code avoids eval, for security.
-
-version = {}
-with open(path.join(here, 'eprints2bags/__version__.py')) as f:
-    text = f.read().rstrip().splitlines()
-    vars = [line for line in text if line.startswith('__') and '=' in line]
-    for v in vars:
-        setting = v.split('=')
-        version[setting[0].strip()] = setting[1].strip().replace("'", '')
-
-# Finally, define our namesake.
-
 setup(
-    name             = version['__title__'].lower(),
-    description      = version['__description__'],
-    long_description = readme,
-    version          = version['__version__'],
-    url              = version['__url__'],
-    author           = version['__author__'],
-    author_email     = version['__email__'],
-    license          = version['__license__'],
-    keywords         = "EPrints archives preservation BagIt",
-    packages         = ['eprints2bags'],
-    scripts          = ['bin/eprints2bags'],
+    setup_cfg        = True,
     install_requires = reqs,
-    platforms        = 'any',
-    python_requires  = '>=3',
 )
