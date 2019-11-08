1a61fcf601d2973b2d8e0d3cff9114d884fc1a0b
Michael Hucka <mhucka@caltech.edu>
2019-11-07 20:49:46 -0800

    Use latest version of my metadata extraction code

--- eprints2bags/__init__.py
+++ eprints2bags/__init__.py
@@ -22,28 +22,52 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-import os
-from   os import path
-from   setuptools.config import read_configuration
+from os import path
 import sys
 
 # Set module-level dunder variables like __version__
 # .............................................................................
-# This code reads metadata values from ../setup.cfg and sets the corresponding
-# module-level variables with '__' surrounding their names.  This approach
-# avoids putting version and other info in more than one place.
+# The following code reads from either ../setup.cfg (if running from a source
+# directory) or data/setup.cfg (if running from an application created by
+# PyInstaller using our PyInstaller configuration scheme), or the installed
+# package metadata (if installed normally).  It reads config vars and sets the
+# corresponding module-level variables with '__' surrounding their names.
 
 keys = ['version', 'description', 'license', 'url', 'keywords',
         'author', 'author_email', 'maintainer', 'maintainer_email']
 
-try:
-    here        = path.abspath(path.dirname(__file__))
-    conf_dict   = read_configuration(path.join(here, '..', 'setup.cfg'))
-    conf        = conf_dict['metadata']
+this_module = sys.modules[__package__]
+module_path = this_module.__path__[0]
+setup_cfg = path.join(module_path, 'data/setup.cfg')
+if not path.exists(setup_cfg):
+    setup_cfg = path.join(path.dirname(__file__), '..', 'setup.cfg')
+
+if path.exists(setup_cfg):
+    # If setup.cfg is directly available, use that.
+    from setuptools.config import read_configuration
+    conf_dict = read_configuration(setup_cfg)
+    conf = conf_dict['metadata']
+    for name in [key for key in keys if key in conf]:
+        variable_name = '__' + name + '__'
+        setattr(this_module, variable_name, str(conf[name]))
+else:
+    # If we are not running from the source directory, we read from the
+    # package metadata file created by setuptools.
+    import distutils.dist, io, pkg_resources
+    pkg = pkg_resources.get_distribution(__package__)
+    metadata = distutils.dist.DistributionMetadata()
+    metadata.read_pkg_file(io.StringIO(pkg.get_metadata(pkg.PKG_INFO)))
+    for name in [key for key in keys if hasattr(metadata, key)]:
+        variable_name = '__' + name + '__'
+        setattr(this_module, variable_name, getattr(metadata, name))
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
     this_module = sys.modules[__package__]
-    for key in keys:
-        if key in conf:
-            variable_name = '__' + key + '__'
-            setattr(this_module, variable_name, str(conf[key]))
-except:
-    pass
+    print('{} version {}'.format(this_module.__name__, this_module.__version__))
+    print('Authors: {}'.format(this_module.__author__))
+    print('URL: {}'.format(this_module.__url__))
+    print('License: {}'.format(this_module.__license__))
