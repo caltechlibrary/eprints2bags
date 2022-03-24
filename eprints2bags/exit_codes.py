fa91d414220ad3fbb3f26557f6a9e1f0fa7c4cc2
mhucka <mhucka@caltech.edu>
2022-03-24 12:14:48 -0700

    Use improved enum approach

--- eprints2bags/exit_codes.py
+++ eprints2bags/exit_codes.py
@@ -14,13 +14,30 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from enum import IntEnum
+from aenum import Enum, MultiValue
 
-class ExitCode(IntEnum):
-    success        = 0
-    user_interrupt = 1
-    bad_arg        = 2
-    no_network     = 3
-    file_error     = 4
-    server_error   = 5
-    exception      = 6
+# I adapted the clever approach posted by the author of the Python aenum
+# package, Ethan Furman, to Stack Overflow on 2016-03-13 at
+# https://stackoverflow.com/a/35964875/743730
+# The most important bit is realizing you can define __int__().
+
+class ExitCode(Enum):
+    '''Class of exit codes that this program may return.
+
+    The numeric value of a given code can be obtained by using int().  For
+    example, int(ExitCode.success) will produce 0.
+    '''
+
+    _init_ = 'value meaning'
+    _settings_ = MultiValue
+
+    success        = 0, "success -- program completed normally"
+    user_interrupt = 1, "the user interrupted the program's execution"
+    bad_arg        = 2, "encountered a bad or missing value for an option"
+    no_network     = 3, "no network detected -- cannot proceed"
+    file_error     = 4, "file error -- encountered a problem with a file or directory"
+    server_error   = 5, "server error -- encountered a problem with the server"
+    exception      = 6, "an exception or fatal error occurred"
+
+    def __int__(self):
+        return self.value
